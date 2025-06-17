7a8c97781cb9245f093db4df319d085967f7a231
kalgo4444 <abuxyziabd@gmail.com>
2025-06-18 00:06:39 +0500

    add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     // header
     const loginButtonEl = document.querySelector("#loginButton")
     const registerButtonEl = document.querySelector("#registerButton")
@@ -7,6 +7,7 @@ addEventListener("DOMContentLoaded", () => {
     const loginModalEl = document.querySelector("#loginModal")
     const userEmailLogEl = document.querySelector("#userEmailLog")
     const userPasswordLogEl = document.querySelector("#userPasswordLog")
+    const forResEl = document.querySelector("#forRes")
     //button
     const loginBtnEl = document.querySelector("#loginBtn")
     const cancerBtnLogEl = document.querySelector("#cancerBtnLog")
@@ -21,13 +22,15 @@ addEventListener("DOMContentLoaded", () => {
     const registerBtnEl = document.querySelector("#registerBtn")
     const cancerBtnRegEl = document.querySelector("#cancerBtnReg")
 
-    //form button preventDefault
+
+    // form button preventDefault
     loginBtnEl.addEventListener("click", (e) => {
         e.preventDefault()
     })
     registerBtnEl.addEventListener("click", (e) => {
         e.preventDefault()
     })
+
 
     //open modal
     //login
@@ -51,4 +54,47 @@ addEventListener("DOMContentLoaded", () => {
     cancerBtnRegEl.addEventListener("click", () => {
         registerModalEl.classList.add("hidden")
     })
+
+
+    // take a value input register and set to storage
+    registerBtnEl.addEventListener("click", () => {
+        const userName = userNameEl.value
+        const userEmail = userEmailEl.value
+        const userPasswd = userPasswordEl.value
+        if (!userName.trim()) return null
+        if (!userEmail.trim()) return null
+        if (!userPasswd.trim()) return null
+        localStorage.setItem("name", userNameEl.value.toString())
+        localStorage.setItem("email", userEmailEl.value.toString())
+        localStorage.setItem("password", userPasswordEl.value.toString())
+        const text = document.createElement("h1")
+        text.textContent = `Hello ${userName}`
+        text.className = "text-center text-2xl mt-10"
+        document.body.appendChild(text)
+        registerModalEl.classList.add("hidden")
+    })
+
+    // take a value input login and much them
+    loginBtnEl.addEventListener("click", () => {
+        const userEmail = userEmailLogEl.value
+        const userPassword = userPasswordLogEl.value
+        const userRegEmail = localStorage.getItem("email")
+        const userRagPasswd = localStorage.getItem("password")
+        if (!userEmail.trim()) return null
+        if (!userPassword.trim()) return null
+        if (userEmail === userRegEmail && userPassword) {
+            forResEl.textContent = "You are log-in"
+            setTimeout(() => {
+                loginModalEl.classList.add("hidden")
+            }, 1000)
+        } else {
+            forResEl.textContent = "Email or Password is mistake"
+            setTimeout(() => {
+                forResEl.textContent = ""
+            }, 3000)
+        }
+        userEmailLogEl.value = ""
+        userPasswordLogEl.value = ""
+    })
+
 })
